0d487ad516ca6af699521c0198f3c427b5559020
AV599625 <Avaratharajan6525@conestogac.on.ca>
2023-02-01 16:29:39 -0500

    perfect

--- task2.py
+++ task2.py
@@ -1,14 +1,16 @@
-#Make sure follow proper format Author date purpose
-#
-#
+#PSUEDO CODE for Task 2
+# Display a user greeting for the calculator
+#Create a variable to store user's input, at the same time we can invoke a strip function to remove the white spaces before and after.
+# Use an if statement to seperate the two possiblities of having either a numeric value or a letter value
+#Create our first scenairo by utilizing an if statement and using the isalpha function to filter out an answer with letters and print an error message notifing the user of the incorrect response
+#Create our last scenario by utilizing our else statement. Now we will NEST four seperate if statements that will correspond to each of our possible choices from 1 to 4
+#We will utilize the import function to call pi within the NESTED if shapeSelection == 1 statement.
 #
 print("Welcome to the Geometry Calculator")
 shapeSelection = input("1. Calculate the Area of a Circle \n2. Calculate the Area of a Rectangle \n3. Calculate the Area of a Triangle \n4. Quit").strip()
-if shapeSelection.isalpha() :
+if shapeSelection.isnumeric() == False :
     print("Values 1 to 4 only, no letters.")
-   
-
-else:
+else :
     shapeSelection = int(shapeSelection)
     if shapeSelection == 1:
         print("C")
